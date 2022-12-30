f78eb2ce0263f5cb109531d4dcde1e276086ac59
Dalex994 <dalex.laville@protonmail.com>
2022-12-30 18:53:44 +0100

    fixing crash of the function

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(t):
+    return 2/(1+t)
+
+a = 0
+b = 10
+n = 20
+h = (b-a)/n
+
+abs = [a+i*h for i in range(n+1)]
+ord = [f(t) for t in abs]
 
 def Liste(x,y,range_b,L,dir_coeffs):
     L = []
 
     for ecart_b in range(0, b):
-        b2= ecart_b+2
-        x= abs+b2
-        y= [f(t) for t in x]
+        b2 = ecart_b+2
+        x = abs+b2
+        y = (f(t) for t in x)
 
-dir_coeffs = np.gradient(y, x)
+    dir_coeffs = np.gradient(y, x)
 
-L.append(dir_coeffs)
-
-show(L)
+    L.append(dir_coeffs)
+    show(L)
