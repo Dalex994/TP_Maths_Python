ffb19f224c97aa3785607d502e8c21c15d7ac552
Dalex994 <dalex.laville@protonmail.com>
2023-11-26 15:53:42 +0100

    hh

--- tpm.py
+++ tpm.py
@@ -1,29 +1,22 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 def f(t):
     return 2/(1+t)
 
 a=0
 b=10
-n=20
+n=30
 h= (b-a)/n
+def coeffs_dirr(pas):
+    ret = []
+    i = 0.5
+    while i<10:
+        ret.append((f(i+0.001)-f(i))/0.001)
+        i+= pas
+    return ret
 
 abs=[a+i*h for i in range(n+1)]
 ord= [f(t) for t in abs]
+print(coeffs_dirr(1 ))
 plt.plot(abs,ord, 'b')
 plt.show()
-
-def Liste(x,y,range_b,L,dir_coeffs):
-    L = []
-
-    for ecart_b in range(0, b):
-        b2= ecart_b+2
-        x= abs+b2
-        y= [f(t) for t in x]
-
-dir_coeffs = np.gradient(y, x)
-
-L.append(dir_coeffs)
-
-show(L)
